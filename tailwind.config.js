65c48c5b44189db8d12ac01b9dfb63f51d483b9b
Svitlana Mishchuk <svitlana@igtechn.com>
2022-09-26 13:01:16 +0200

    styles(app): added Souvenir font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
+    fontFamily: {
+      souv: ['Souvenir', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
